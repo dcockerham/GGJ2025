33f521793210f0f6c9f30647ce3264593e13a7b2
dylantchaos <dylantchaos@gmail.com>
2025-01-25 18:40:15 -0800

    - anglerfish chomp now - depth counter in progress - refining fishy stage a bit

---
+++ Assets/Scripts/FrameCycler.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class FrameCycler : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private float minLength = 1.0f;
+    [SerializeField] private float maxLength = 4.0f;
+    float cycleTimer;
+    protected SpriteRenderer m_Sprite;
+    int currentIndex;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_Sprite = GetComponent<SpriteRenderer>();
+        if (sprites.Length != 0)
+        {
+            currentIndex = Random.Range(0, sprites.Length);
+            m_Sprite.sprite = sprites[currentIndex];
+        }
+        cycleTimer = Random.Range(minLength, maxLength);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cycleTimer -= Time.deltaTime;
+        if (cycleTimer < 0.0f)
+        {
+            cycleTimer = Random.Range(minLength, maxLength);
+            if (++currentIndex > sprites.Length)
+            {
+                currentIndex = 0;
+            }
+            m_Sprite.sprite = sprites[currentIndex];
+        }
+    }
+}
